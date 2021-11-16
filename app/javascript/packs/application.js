485a485768a0ae5440295ebdcb616a451917951b
Tiago Collot <collot.tiago1@gmail.com>
2021-11-16 17:57:46 +0000

    Rails Task Manager

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
-
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
